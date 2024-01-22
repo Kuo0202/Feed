4aaea0ea4cf6ba5f37c6e47600fb226991486c60
Kuo0202 <bryant01020304@gmail.com>
2024-01-22 16:58:30 +0800

    Request data from ‘load’

--- Feed/FeedTests/RemoteFeedLoaderTests.swift
+++ Feed/FeedTests/RemoteFeedLoaderTests.swift
@@ -7,17 +7,33 @@
 
 import XCTest
 
-class RemoteFeedLoader {}
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestURL = URL(string: "Https://url.com")
+    }
+}
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+
     var requestURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
 
         XCTAssertNil(client.requestURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestURL)
     }
 }
